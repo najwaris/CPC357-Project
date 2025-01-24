21e69f84857d9899615112d441f56388ea74d7ea
nwaee <122419185+najwaris@users.noreply.github.com>
2025-01-24 20:15:33 +0800

    Create vonesetting.h

---
+++ code/vonesetting.h
@@ -0,0 +1,17 @@
+#if !defined (VONESETTING_H)
+#define VONESETTING_H
+#else
+#error Multiple includes of vonesetting.h
+#endif
+
+#define WIFI_SSID "yourWiFiSSID"          //Replace this with YOUR WiFi SSID
+#define WIFI_PASSWORD "yourWiFipassword"  //Replace this with YOUR WiFi Password
+
+#define MQTT_SERVER "mqtt.v-one.my"
+#define MQTT_PORT 8883
+#define MQTT_USERNAME "yourGatewayToke"  //Replace this with the Access Token of YOUR gateway
+#define MQTT_PASSWORD " "
+
+#define GATEWAYID "yourGatewayID"  //Replace this with the gatewayID of your gateway 
+#define INTERVAL 1000 //1S
+#define INTERVAL2 500 //0.5S
